70069aa7e52c46b4303aa5586a9fa6cf20f40c0a
Narendra Nag <narendranag@gmail.com>
2024-10-14 20:19:27 -0500

    Euler 18 — Max Path Sum

--- 18-max-path-sum.py
+++ 18-max-path-sum.py
@@ -1,67 +1,45 @@
-'''
-Find the maximum total from top to bottom
 
-    3
-   7 4
-  2 4 6
- 8 5 9 3
+# Problem: Maximum path sum I
+# By starting at the top of the triangle below and moving to adjacent numbers on the row below,
+# the maximum total from top to bottom is 23.
+# 3
+# 7 4
+# 2 4 6
+# 8 5 9 3
+# That is, 3 + 7 + 4 + 9 = 23.
+# Find the maximum total from top to bottom of the triangle below:
+# 75
+# 95 64
+# 17 47 82
+# 18 35 87 10
+# 20 04 82 47 65
+# 19 01 23 75 03 34
+# 88 02 77 73 07 63 67
+# 99 65 04 28 06 16 70 92
+# 41 41 26 56 83 40 80 70 33
+# 41 48 72 33 47 32 37 16 94 29
+# 53 71 44 65 25 43 91 52 97 51 14
+# 70 11 33 28 77 73 17 78 39 68 17 57
+# 91 71 52 38 17 14 91 43 58 50 27 29 48
+# 63 66 04 68 89 53 67 30 73 16 69 87 40 31
+# 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
 
-OR
+# Solution:
+# The key to this problem is to start from the bottom of the triangle and work our way up.
+# We will start from the second last row and add the maximum of the two adjacent numbers to the current number.
+# We will continue this process until we reach the top of the triangle.
+# The maximum sum will be the number at the top of the triangle.
 
-3
-7 4
-2 4 6
-8 5 9 3
+def max_path_sum(triangle):
+    for i in range(len(triangle) - 2, -1, -1):
+        for j in range(len(triangle[i])):
+            triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
+    return triangle[0][0]
 
-
-Let's break this down
-
-row 0 has 1 number
-row 1 has 2 numbers
-...
-row 4 has 5 mumbers
-
-or row n has n+1 numbers
-
-In four rows there are 8 (2 x total number of rows) sums
-
-first sequence
-
-1,1 > 2,1 > 3,1 > 4,1 = 3 + 7 + 2 + 8 = 20
-
-1,1 > 2,1 > 3,1 > 4,2 = 3 + 7 + 2 + 5 = 17
-1,1 > 2,1 > 3,2 > 4,2 = 3 + 7 + 4 + 5 = 19
-1,1 > 2,2 > 3,2 > 4,2 = 3 + 4 + 4 + 5 = 16
-
-1,1 > 2,1 > 3,2 > 4,3 = 3 + 7 + 4 + 9 = 23
-1,1 > 2,2 > 3,2 > 4,3 = 3 + 4 + 4 + 9 = 20
-1,1 > 2,2 > 3,3 > 4,3 = 3 + 4 + 6 + 9 = 22
-
-
-1,1 > 2,2 > 3,3 > 4,4 = 3 + 4 + 6 + 3 = 16
-
-'''
-
-triangle = [3], [7,4], [2,4,6], [8,5,9,3]
-
-iteration, i,j = 0,0,0
-multiplier = 0
-sum[i] = 0
-
-while(iteration < 13):
-    sum[i] += triangle[i][j]
-    i += 1
-    if(i == 4):
-        multiplier += 1
-
-    j += (multiplier * i)
-
-
-
-
-
-
-
-
-
-
+triangle = [
+    [3],
+    [7, 4],
+    [2, 4, 6],
+    [8, 5, 9, 3]
+]
+print(max_path_sum(triangle))
